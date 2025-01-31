d6abfe1c06334295b2e4aff750f5113cd8cfbe1d
lazymonkey699 <abhishek.bam10@gmail.com>
2025-01-31 14:13:36 +0545

    Done without error

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer'
 import LandingPage from './components/LandingPage/LandingPage'
+import FeaturedSection from './components/FeatureSection/FeaturedSection'
 
 const App = () => {
   return (
     <div>
         <Navbar/>
         <LandingPage/>
+        <FeaturedSection/>
 
         <Footer/>
     </div>
---
+++ src/data.js
@@ -0,0 +1,20 @@
+// src/data/destinations.js
+
+export const destinations = [
+    {
+      name: "Kot Dada",
+      description: "Despite being a beautiful height Kot dada is still unknown to many outdoor as well as indoor travellers. Kot dada is a famous height located at southern part of Kathmandu valley. From the top of the place you can have a beautiful view of all three cities of Kathmandu valley i.e. Kathmandu, Lalitpur and Bhaktapur which of course will cherish your heart and soul.",
+      image: "/assets/kot-dada.jpg" // Only one image per destination
+    },
+    {
+      name: "Tarebhir",
+      description: "Tarebhir offers magnificent views of Kathmandu Valley, walk into the pine forest and open picnic spot. On the other hand, there are some places to eat and no need to worry and carry food with you. Also, this hike can combine with other hiking routes if you have a longer time to explore beautiful Nature. This is one of the best spots for overnight camping nearby Kathmandu.",
+      image: "/assets/Tarebhir.jpg"
+    },
+    {
+      name: "Champadevi ",
+      description: "Champadevi is a popular hiking destination near Kathmandu, Nepal. It is the third highest hill in the Kathmandu Valley, reaching an elevation of 2,278 meters above sea level. The hike to Champadevi is well-maintained and suitable for hikers of all ages and fitness levels, making it a great option for a day trip from Kathmandu.",
+      image: "/assets/champadevi.jpg"
+    }
+  ];
+  
