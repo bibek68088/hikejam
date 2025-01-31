30dc25969f3c7f83324ceaeb8f6e9c7365462ad2
bibek88 <bibeks337@gmail.com>
2025-01-31 14:41:29 +0545

    added base for the mid contents

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer'
 import LandingPage from './components/LandingPage/LandingPage'
+import TopContent from './components/Contents/components/TopContent'
 
 const App = () => {
   return (
     <div>
         <Navbar/>
         <LandingPage/>
-
+        <TopContent />
         <Footer/>
     </div>
   )
